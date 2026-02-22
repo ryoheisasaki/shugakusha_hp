ef634908dea0aeb6527410a56983ce10e1f6f3b8
ryoheisasaki <soltpeppersolt@softbank.ne.jp>
2026-02-22 14:07:08 +0900

    構成整理 & Laravel追加

---
+++ main_page/main_page.php
@@ -0,0 +1,14 @@
+<?php
+$a = 123; // ←この行の左をクリックして赤丸（ブレークポイント）
+echo "hello2";
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
+$hoge = 1;
